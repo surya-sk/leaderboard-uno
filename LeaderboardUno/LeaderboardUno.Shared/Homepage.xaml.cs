d8419d84e45a5799be56d66855ec5ec179789fdd
Surya <surya.sk05@outlook.com>
2020-09-01 14:07:22 +0530

    Add sumbit game button

--- LeaderboardUno/LeaderboardUno.Shared/Homepage.xaml.cs
+++ LeaderboardUno/LeaderboardUno.Shared/Homepage.xaml.cs
@@ -22,16 +22,12 @@ namespace LeaderboardUno.Shared
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class Homepage : Page, IGamesUpdateable
+    public sealed partial class Homepage : Page
     {
-        string GameName, Type;
-        double Num;
-        private ObservableCollection<PlayerStat> PlayerStats;
-        private ObservableCollection<Game> games;
+       
         public Homepage()
         {
             this.InitializeComponent();
-            PlayerStats = PlayerStatList.GetPlayerStats();
             
         }
 
@@ -41,70 +37,34 @@ namespace LeaderboardUno.Shared
             GameDetPanel.Visibility = Visibility.Visible;
         }
 
+        private void GameType_Checked(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+        }
+
         private void CreateGameButton_Click(object sender, RoutedEventArgs e)
         {
-            GameName = NameInput.Text;
-            string temp = NumInput.Text;
-            Num = Convert.ToDouble(temp);
-            CreatePlayerStats();
-            InputPlayers();
+            //GameName = NameInput.Text;
+            //Type = GameType.SelectedItem.ToString();
+            //string temp = NumInput.Text;
+            //Num = Convert.ToDouble(temp);
+            //CreatePlayerStats();
+            //InputPlayers();
             //this.Frame.Navigate(typeof(Homepage));
             //this.Frame.Navigate(typeof(Leaderboard));
-        }
-
-        private void CreatePlayerStats()
-        {
-            for (int i = 0; i < Num; i++)
-            {
-                PlayerStats.Add(new PlayerStat { PlayerName = "", PlayerScore = 0 });
-            }
-        }
-
-        private async void InputPlayers()
-        {
-            ContentDialogResult result = await NamesDialog.ShowAsync();
-        }
-
-        private void NamesDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
-        {
-            Game game = new Game() { id = Guid.NewGuid(), GameName = GameName, GameType = Type, NumPlayers = Num, PlayerStatList = PlayerStats };
-            Profile.GetInstance().AddGame(game);
-            InputGame.Visibility = Visibility.Visible;
             GameDetPanel.Visibility = Visibility.Collapsed;
         }
 
-        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        private void NumInput_BeforeTextChanging(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
         {
-           // this.Frame.Navigate(typeof(Settings));
-        }
-
-        private void HelpButton_Click(object sender, RoutedEventArgs e)
-        {
-            //this.Frame.Navigate(typeof(HelpPage));
-        }
-        private void RadioButtonChecked(object sender, RoutedEventArgs e)
-        {
-            RadioButton rb = sender as RadioButton;
-            Type = rb.Name;
-        }
-
-        public void UpdateGames(ObservableCollection<Game> games)
-        {
-            this.games = games;
-            Console.WriteLine("Homepage" + games.Count);
-        }
-
-        private void NumInput_Validate(TextBox sender, TextBoxBeforeTextChangingEventArgs args)
-        {
-            //args.Cancel = args.NewText.Any(char => !char.IsDigit(c));
             foreach (char c in args.NewText)
             {
                 if (!char.IsDigit(c))
                 {
-
                     args.Cancel = true;
                 }
             }
         }
+
     }
 }
